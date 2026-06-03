a8856c5a16dac431f45f31d02049f313c2baff39
Juan Silva <juan.silva.r@uni.pe>
2026-06-03 14:53:46 -0500

    feat: clase sabado que se faltó, código de Bobbio

--- Common/src/main/java/pe/edu/uni/fc/cc/common/Constants.java
+++ Common/src/main/java/pe/edu/uni/fc/cc/common/Constants.java
@@ -26,6 +26,8 @@ public class Constants {
      public static final String USER_HOME = System.getProperty("user.home");
      public static final String USER_CD_FILENAME = "C:\\UNI\\Cursos\\CC0F7 - Topicos-de-Ciencia-de-la-Computacion-IV\\sem-9\\user-cd.crt";
      public static final String USER_CD_FILENAME2 = Paths.get(USER_HOME, "user-cd.crt").toString();
+     public static final String CA_ROOT_CONTAINER_FILENAME = Paths.get(USER_HOME, "ca-root-container.p12").toString();
+     public static final String CA_ROOT_ALIAS = "ca_root_alias";
      
      public static final int AES_CBC_IV_LENGTH = 16;
      public static final int AES_GCM_IV_LENGTH = 12;
@@ -36,6 +38,8 @@ public class Constants {
      public static final String ECC_KEY_PARAM = "secp256r1";
      public static final String ECC_SIGN_ALGORITHM = "SHA256withECDSA"; // ALGORITMO PARA FIRMA
      public static final String RSA_SIGN_ALGORITHM = "SHA256WithRSA";
+     
+     public static final String END_USER_ALIAS = "juan";
      
      public static final int TAG_LENGTH = 128;
 }
